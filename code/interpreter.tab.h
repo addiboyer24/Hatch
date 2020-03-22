82f310d2703f75e65b11c25ddc65afdebf26908f
addiboyer24 <Addison.Boyer@UMontana.edu>
2020-03-22 15:58:10 -0600

    Adding nicer folder structure

---
+++ code/interpreter.tab.h
@@ -0,0 +1,93 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_INTERPRETER_TAB_H_INCLUDED
+# define YY_YY_INTERPRETER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    CLASS = 258,
+    COMMA = 259,
+    EQUALS = 260,
+    EXIT = 261,
+    EOL = 262,
+    GET = 263,
+    STR = 264,
+    STRING = 265,
+    INDENT = 266,
+    LPAREN = 267,
+    NUM_F = 268,
+    NUM_I = 269,
+    PYTHONLINE = 270,
+    RPAREN = 271,
+    SET = 272,
+    START = 273,
+    VARNAME = 274
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 189 "interpreter.y" /* yacc.c:1909  */
+
+
+	int iVal;
+	char* sVal;
+	
+
+#line 81 "interpreter.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_INTERPRETER_TAB_H_INCLUDED  */
